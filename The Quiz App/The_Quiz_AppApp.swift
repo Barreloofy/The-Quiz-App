7b2e9b4f5f9ec47f7c30e525f5eb4576ab69572c
Nils <nilskofodhk@protonmail.com>
2024-07-28 22:47:47 +0200

    Initial Commit

---
+++ The Quiz App/The_Quiz_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Quiz_AppApp.swift
+//  The Quiz App
+//
+//  Created by Nils on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct The_Quiz_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
